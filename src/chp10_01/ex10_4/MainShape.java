3d80b3e835df7b844aae39100109aacb428b6096
wshin025 <wody0506@gmail.com>
2024-10-17 09:46:49 +0900

    업캐스팅: 실습 10-4(ArrayList)

--- src/chp10_01/ex10_4/MainShape.java
+++ src/chp10_01/ex10_4/MainShape.java
@@ -1,5 +1,7 @@
 package chp10_01.ex10_4;
 
+import java.util.ArrayList;
+
 public class MainShape {
     public static void main(String[] args) {
         Square s = new Square();
@@ -10,10 +12,15 @@ public class MainShape {
         c.name="원";
 
         // 이것도 업케스팅 Shape s1 =new Square();
-        Shape[] shapes = {s,t,c};
+        // 배열 Shape[] shapes = {s,t,c}
 
-        for (int i = 0; i < shapes.length; i++) {
-            System.out.printf("인덱스 번호 %d번의 도형이름 : %s\n", i,shapes[i].name);            
+        ArrayList<Shape> shapes = new ArrayList<>();
+        shapes.add(s);
+        shapes.add(t);
+        shapes.add(c);
+
+        for (int i = 0; i < shapes.size(); i++) {
+            System.out.printf("인덱스 번호 %d번의 도형이름 : %s\n", i,shapes.get(i).name);
         }
 
     }
